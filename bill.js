80a0393dda5f8a899402755459d8647900e6d763
SoSarakham <boonchoo082@gmail.com>
2025-09-08 16:59:46 +0700

    Update bill.js

--- bill.js
+++ bill.js
@@ -362,7 +362,6 @@ function buildPrintView(bill) {
       <p>โทร: 0885305228, 0621392902</p>
       <p>บิลเลขที่: ${bill.billno}</p>
       <p>โต๊ะ/ลูกค้า: ${bill.customer || '-'}</p>
-      <p>เวลาเปิดบิล: ${createdText}</p>
       <p>เวลาเช็คบิล: ${closedText}</p>
     </div>
     <table>
