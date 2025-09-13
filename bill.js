3362aec6cf162bf55e4ea6673414c469733d1737
SoSarakham <boonchoo082@gmail.com>
2025-09-13 07:59:47 +0700

    Update bill.js

--- bill.js
+++ bill.js
@@ -75,7 +75,7 @@ async function loadMenu() {
           <input class="num menu-qty" 
             type="text" 
             data-id="${item.id}" 
-            value="${existingValues[item.id] || ''}"
+            value=""
             placeholder="เช่น 1+2"
             inputmode="decimal" 
             pattern="[0-9.+]*">
